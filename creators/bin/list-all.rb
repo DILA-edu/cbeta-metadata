22896827389398422c3be4b07bf805dd0ec715b0
Ray Chou <zhoubx@gmail.com>
2017-03-05 12:11:01 +0800

    fix bug

--- creators/bin/list-all.rb
+++ creators/bin/list-all.rb
@@ -10,9 +10,18 @@ def handle_file(fn)
   creators = JSON.parse(s)
   creators.each do |k, v|
     if v.key? 'creators_with_id'
-      v['creators_with_id'].split(';').each do |c|
+      s = v['creators_with_id']
+      if s.include? ','
+        puts fn
+        puts "creators_with_id 欄位不應有逗點: #{s}" 
+        puts "如果是多個作譯者，應以半形分號隔開"
+        abort
+      end
+      s.split(';').each do |c|
         if c.match(/^(.*?)\((A\d{6})\)$/)
-          $creators[$2]=$1 unless $creators.key? $2
+          name = $1
+          k = $2
+          $creators[k]=name unless $creators.key? k
         else
           puts "#{k} 格式錯誤：#{c}"
         end
